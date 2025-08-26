635509e66b8946b1da6f749487b722dbe7b02820
Vivek Narang <vivek@searchscale.com>
2025-08-26 15:24:52 -0400

    Add licenses

--- solr/core/src/java/org/apache/solr/core/CuvsCodec.java
+++ solr/core/src/java/org/apache/solr/core/CuvsCodec.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.solr.core;
 
 import com.nvidia.cuvs.lucene.Lucene99AcceleratedHNSWVectorsFormat;
@@ -41,7 +57,9 @@ public class CuvsCodec extends FilterCodec {
             Integer.parseInt(args._getStr("graphDegree")),
             Integer.parseInt(args._getStr("hnswLayers")));
 
-    log.info("Created the CuVS Vectors Format: " + cuvsVectorsFormat);
+    if (log.isInfoEnabled()) {
+      log.info("Created the CuVS Vectors Format: {}", cuvsVectorsFormat);
+    }
   }
 
   @Override
@@ -57,7 +75,9 @@ public class CuvsCodec extends FilterCodec {
       // TODO should we have a special field type?
       DenseVectorField vectorType = (DenseVectorField) fieldType;
       String knnAlgorithm = vectorType.getKnnAlgorithm();
-      log.info("The field's algo type is: " + knnAlgorithm);
+      if (log.isInfoEnabled()) {
+        log.info("The field's algo type is: {}", knnAlgorithm);
+      }
       if ("cuvs".equals(knnAlgorithm)) {
         return cuvsVectorsFormat;
       } else if (DenseVectorField.HNSW_ALGORITHM.equals(knnAlgorithm)) {
